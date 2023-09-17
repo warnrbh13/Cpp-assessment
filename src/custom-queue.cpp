b442941a5d550e56c29d508e1fd616e75bd3e207
Benjamin Barbosa <benjamin.barbosah@gmail.com>
2023-09-17 02:22:56 -0600

    Add initial implementation of CustomQueue class, enqueue method implementation

---
+++ src/custom-queue.cpp
@@ -0,0 +1,35 @@
+#include "custom-queue.h"
+#include <cstdio>
+
+CustomQueue::CustomQueue() {}
+
+void CustomQueue::enqueue(ClientRequest& client_request) {
+    if(_clients_count + _servers_count > MAX_SIZE) {printf("[Error] Max size reached\n");}
+    QueueElement* new_element = new QueueElement(client_request);
+    if(_queue_size == size_t{}) {
+        _front = new_element;
+        _top = new_element;
+    }
+    else {
+        _top->next = new_element;
+        _top = new_element;
+    }
+    ++_queue_size;
+}
+
+void CustomQueue::enqueue(ServerResponse& server_response) {
+    if(_clients_count + _servers_count > MAX_SIZE) {printf("[Error] Max size reached\n");}
+    QueueElement* new_element = new QueueElement(server_response);
+    if(_queue_size == size_t{}) {
+        _front = new_element;
+        _top = new_element;
+    }
+    else {
+        _top->next = new_element;
+        _top = new_element;
+    }
+    ++_queue_size;
+}
+
+void CustomQueue::deque() {
+}
