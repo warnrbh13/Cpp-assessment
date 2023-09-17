53e971ef1de0ead5599bedc98accb36298853d5e
Benjamin Barbosa <benjamin.barbosah@gmail.com>
2023-09-17 02:33:58 -0600

    Add missing increment for counters

--- src/custom-queue.cpp
+++ src/custom-queue.cpp
@@ -14,11 +14,16 @@ void CustomQueue::enqueue(ClientRequest& client_request) {
         _top->next = new_element;
         _top = new_element;
     }
+    ++_clients_count;
     ++_queue_size;
 }
 
 void CustomQueue::enqueue(ServerResponse& server_response) {
-    if(_clients_count + _servers_count > MAX_SIZE) {printf("[Error] Max size reached\n");}
+    if(_clients_count + _servers_count > MAX_SIZE) {
+        printf("[Error] Max size reached\n");
+        return;
+    }
+
     QueueElement* new_element = new QueueElement(server_response);
     if(_queue_size == size_t{}) {
         _front = new_element;
@@ -28,6 +33,7 @@ void CustomQueue::enqueue(ServerResponse& server_response) {
         _top->next = new_element;
         _top = new_element;
     }
+    ++_servers_count;
     ++_queue_size;
 }
 
