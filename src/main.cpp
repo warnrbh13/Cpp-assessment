8fe878ac558b475babccf57b3d7e564092e98005
Benjamin Barbosa <benjamin.barbosah@gmail.com>
2023-09-18 01:08:31 -0600

    Add main source file to test CustomQueue

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+#include "client-request.h"
+#include "server-response.h"
+#include "custom-queue.h"
 
 int main() {
+    CustomQueue queue;
+
+    ClientRequest clientRequest1("m1", "C1", 10);
+    ServerResponse serverResponse1("m2", "S1", "4bytes,1second");
+    ClientRequest clientRequest2("m3", "C2", 15);
+
+    queue.enqueue(clientRequest1);
+    queue.enqueue(serverResponse1);
+    queue.enqueue(clientRequest2);
+
+    std::cout << queue.getCountClients() << std::endl; // Output: 2
+    std::cout << queue.getCountServers() << std::endl; // Output: 1
+
+    std::string front = queue.getFront();
+    std::cout << front << std::endl; // Output: "m1 C1 10"
+
+    queue.deque();
+    front = queue.getFront();
+    std::cout << front << std::endl; // Output: "m2 S1 4bytes,1second"
+
     return {};
 }
